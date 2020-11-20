2948b19c3ba11266d36525f8ad4b200254852c45
Rashid Razak <bib.nexus@gmail.com>
2020-11-20 23:45:57 +0800

    Add Promise.all - one rejects

--- src/promise-all-rejected.js
+++ src/promise-all-rejected.js
@@ -4,32 +4,33 @@
   const timeOut = (timeout) => {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
-        resolve(`Promise resolved in ${timeout}`)
+        if (timeout > 2000) {
+          reject(`Promise rejected: ${timeout}`)
+        } else {
+          resolve(`Promise resolved in ${timeout}`)
+        }
       }, timeout)
     })
   }
-
-  /**
-   * Single promise running
-   */
-  timeOut(1000)
-    .then((result) => console.log(result))
-    .catch((error) => console.log(error))
 
   /**
    * Running multiple promises in parallel using Promise.all
    */
   const promises = [timeOut(2000), timeOut(3000), timeOut(3000), timeOut(3000)]
   const t0 = performance.now()
-  const result = await Promise.all(promises)
+  const result = await Promise.all(promises).catch(error => {
+    console.log('Error: ', error)
+    return error
+  })
   const t1 = performance.now()
 
   /**
    * The whole promise will take around 3000ms since the promises are run in parallel
+   * Since one of it rejects, Promise.all will throw en error
    */
   console.log(`Started at ${t0}`)
   console.log(`Ended at ${t1}`)
   console.log(`Took ${t1 - t0}`)
 
-  console.log(result)
+  console.log(result) // Promise rejected: 3000
 })()
